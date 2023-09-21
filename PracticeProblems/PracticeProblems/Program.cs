d0bd9d5461548f3a514eab102a71330f42e25b26
udaykumar721 <uday56367@gmail.com.com>
2023-09-21 19:21:13 +0530

    []Add : Demo Of Class And Object.

---
+++ PracticeProblems/PracticeProblems/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProblems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TestClass test = new TestClass();
+            test.Demo();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ PracticeProblems/PracticeProblems/TestClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProblems
+{
+    internal class TestClass
+    {
+        public void Demo()
+        {
+            Console.WriteLine("hello world");
+        }
+    }
+}
